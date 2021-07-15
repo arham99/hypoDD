7b6c7bbf54346e542f00f001ed2e6b7d77503a0c
arham zakki edelo <azedelo1999@gmail.com>
2021-07-15 23:35:59 +0700

    python program for collecting  from NonLinLoc
    these programs are used to collect statistical data from NonLinLoc program to be further used for hypoDD

---
+++ run.sh
@@ -0,0 +1,14 @@
+echo "============"
+echo "Script to Collect Stattistic result From NonLinLoc - Global mode"
+echo "Developed by Arham Zakki Edelo TG-ITS"
+echo "Email: azedelo1999@gmail.com"
+echo
+echo "IMPORTANT"
+echo "   1. Taruh SH dan  Script stat_event.py pada directory yang sama dengan data yang akan di collect"
+echo "   2. Harus terinstall python3 dengan library pandas "
+echo
+
+#PS_VIEWER=ghostview
+
+echo
+python3 ./stat_event.py
