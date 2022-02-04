872bb87d0a23e7e0da0cc965805c5230e4128dc9
arham zakki edelo <azedelo1999@gmail.com>
2022-02-04 20:04:31 +0700

    Creatinh new python file test

---
+++ bgjx.py
@@ -0,0 +1,10 @@
+print("hello world!")
+
+class Nambah():
+  def__init__(self, a, b):
+    self.a = a
+    self.b = b
+  def Hasil(self):
+    self.result = self.a + self.b
+    return("Hasil penjumlahannya adalah {}".format(self.result))
+  
